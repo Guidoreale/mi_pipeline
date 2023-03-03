67dca3497981aab77077957f3d190084ecbfbb52
Guido <Guido@DESKTOP-2QGGVKP>
2023-03-03 14:26:13 -0300

    pipeline v2.0.2

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import os
 
 def main():
     """chequea funcionalidad"""
-    os.makedirs("build")
+    if not os.path.exists("build"):
+        os.makedirs("build")
 
     with open("build/index.html", 'w', encoding="utf-8") as var:
         var.write("<html><body><h1>mi propio pipeline<h1><body><html>")
